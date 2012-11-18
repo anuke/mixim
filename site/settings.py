a6bd8971d3d64a4c6de938124a5503b68b030777
Sergey Antonyuk <autonuke@gmail.com>
2012-11-18 18:14:31 +0400

    Moved the application to the mixim.ru.

--- site/settings.py
+++ site/settings.py
@@ -35,7 +35,7 @@ LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-SITE_DIR = '/var/www/html/api2.idrop.ru/'
+SITE_DIR = '/var/www/html/mixim.ru/'
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -52,7 +52,7 @@ MEDIA_ROOT = SITE_DIR + 'upload/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://api2.idrop.ru/media/'
+MEDIA_URL = 'http://mixim.ru/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
