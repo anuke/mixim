b8679e5f85c893b7928f6de306e9fb8bd099b68d
Sergey Antonyuk <autonuke@gmail.com>
2013-05-15 19:54:01 +0400

    Render body template to message body.

--- site/server/utils.py
+++ site/server/utils.py
@@ -8,5 +8,5 @@ def send_mail_to_user(user, template, context = None):
     context['user'] = user
 
     send_mail(render_to_string('mail/%s.subject' % template, context).strip(),
-        render_to_string('mail/%s.subject' % template, context),
+        render_to_string('mail/%s.body' % template, context),
         None, [user.email])
