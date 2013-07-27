6f2d0a559316b52569f12cc7027b22fa9a7ce046
Sergey Antonyuk <autonuke@gmail.com>
2013-07-27 19:53:11 +0400

    Refs #64. Uses current species.

--- site/server/views.py
+++ site/server/views.py
@@ -483,7 +483,7 @@ def __list_media(request, query, start, limit):
     pet_id    = request.GET.get('pet_id')
     breed     = request.GET.get('breed')
     gender    = request.GET.get('gender')
-    species   = request.GET.get('species')
+    species   = current_species(request)
 
     query = query.order_by('-created')
     if tags:
