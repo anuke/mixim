7d419751b12459b96936462f09306aeb8f922e42
Sergey Antonyuk <autonuke@gmail.com>
2013-09-10 11:05:27 +0400

    refs #77 добавил первые модели, чтобы Арутр мог работать

--- site/server/admin.py
+++ site/server/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from server.models import UserProfile
+from server.models import *
 
 
 class UserProfileInline(admin.StackedInline):
@@ -13,6 +13,11 @@ class UserProfileInline(admin.StackedInline):
 class UserAdmin(UserAdmin):
     inlines = (UserProfileInline, )
 
+
+admin.site.register(Pet)
+admin.site.register(Breed)
+admin.site.register(MediaFile)
+admin.site.register(Comment)
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
