42e90f41dd8ef9b1bc81e7b6771840786033603a
Sergey Antonyuk <autonuke@gmail.com>
2013-11-10 23:01:05 +0400

    Refs #109. Uses values dictionary separately.

--- site/server/utils.py
+++ site/server/utils.py
@@ -8,7 +8,8 @@ def send_mail_to_user(request, user, template, context = None):
 
     context['user'] = user
 
-    request_context = RequestContext(request, context)
-    send_mail(render_to_string('mail/%s.subject' % template, request_context).strip(),
-        render_to_string('mail/%s.body' % template, request_context),
+    request_context = RequestContext(request)
+    send_mail(
+        render_to_string('mail/%s.subject' % template, context, request_context).strip(),
+        render_to_string('mail/%s.body' % template, context, request_context),
         None, [user.email])
