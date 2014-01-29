af94a10214c068be34f9b4c9793c1f30b2d044e1
Sergey Antonyuk <autonuke@gmail.com>
2014-01-29 11:47:15 +0400

    refs #210 заменил resized images to thumbnail

--- static/js/app/api.js
+++ static/js/app/api.js
@@ -77,9 +77,9 @@ function show_photo(picId) {
 
                     $('#code1').html(photo.original.replace('original', 'resized/598x598'))
                     $('#code2').text('<img src="' + photo.original.replace('original', 'resized/598x598') + '"/>')
-                    $('#code3').text('<a href="' + photo.original + '"><img src="' + photo.original.replace('original', 'resized/598x598') + '"/></a>')
+                    $('#code3').text('<a href="' + photo.original + '"><img src="' + photo.thumbnail '"/></a>')
                     $('#code4').text('[IMG]' + photo.original + '[/IMG]')              
-                    $('#code5').text('[URL=' + photo.original + '][IMG]' + photo.original.replace('original', 'resized/598x598') + '[/IMG][/URL]')
+                    $('#code5').text('[URL=' + photo.original + '][IMG]' + photo.thumbnail + '[/IMG][/URL]')
                     $('#code6').text('http://mixim.ru/pic' + photo.id )
 
                     // Share section
