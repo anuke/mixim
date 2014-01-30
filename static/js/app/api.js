b7dd1c55dfd248c669625c9a05ff6a4b3e504429
Sergey Antonyuk <autonuke@gmail.com>
2014-01-30 23:11:34 +0400

    Refs #210. Sets value instead of text to share links to picture.

--- static/js/app/api.js
+++ static/js/app/api.js
@@ -76,11 +76,11 @@ function show_photo(picId) {
                     }
 
                     $('#code1').html(photo.original.replace('original', 'resized/598x598'))
-                    $('#code2').text('<img src="' + photo.original.replace('original', 'resized/598x598') + '"/>')
-                    $('#code3').text('<a href="' + photo.original + '"><img src="' + photo.thumbnail + '"/></a>')
-                    $('#code4').text('[IMG]' + photo.original + '[/IMG]')
-                    $('#code5').text('[URL=' + photo.original + '][IMG]' + photo.thumbnail + '[/IMG][/URL]')
-                    $('#code6').text('http://' + window.location.hostname + '/pic' + photo.id )
+                    $('#code2').val('<img src="' + photo.original.replace('original', 'resized/598x598') + '"/>')
+                    $('#code3').val('<a href="' + photo.original + '"><img src="' + photo.thumbnail + '"/></a>')
+                    $('#code4').val('[IMG]' + photo.original + '[/IMG]')
+                    $('#code5').val('[URL=' + photo.original + '][IMG]' + photo.thumbnail + '[/IMG][/URL]')
+                    $('#code6').val('http://' + window.location.hostname + '/pic' + photo.id )
 
                     // Share section
 
