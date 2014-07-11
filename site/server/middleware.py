03b53879bdd6424c95c4dbb6376bbae68c7b235c
Sergey Antonyuk <autonuke@gmail.com>
2014-07-12 01:25:49 +0400

    Ref #000. Code style changes

--- site/server/middleware.py
+++ site/server/middleware.py
@@ -7,15 +7,15 @@ _locals = local()
 
 class HostnameMiddleware:
     def process_request(self, request):
-            hostname = request.META['HTTP_HOST']
-            request.hostname = hostname
+        hostname = request.META['HTTP_HOST']
+        request.hostname = hostname
 
-            if hostname.endswith(".us"):
-                request.session['django_language'] = 'en'
-                request.domain_country = 'US'
-            elif hostname.endswith(".ru"):
-                request.session['django_language'] = 'ru'
-                request.domain_country = 'RU'
+        if hostname.endswith(".us"):
+            request.session['django_language'] = 'en'
+            request.domain_country = 'US'
+        elif hostname.endswith(".ru"):
+            request.session['django_language'] = 'ru'
+            request.domain_country = 'RU'
 
 
 # Request middleware
